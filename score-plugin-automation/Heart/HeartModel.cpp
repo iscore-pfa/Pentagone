0a681631f7f6d6cc104bde126b90e9d1780732b0
Anthony <anthofreyer@gmail.com>
2020-02-19 12:54:23 +0100

    Heart models

--- score-plugin-automation/Heart/HeartModel.cpp
+++ score-plugin-automation/Heart/HeartModel.cpp
@@ -24,38 +24,20 @@ ProcessModel::ProcessModel(
     , outlet{Process::make_outlet(Id<Process::Port>(0), this)}
 {
 
-  outlet->type = Process::PortType::Message;
-  //points p1 =  ({0., 0.});
+  //outlet->type = Process::PortType::Message;
+  m_spline.points.push_back({0.5, 0.});
 
-  //m_spline.points.push_back({0., 0.});
+m_spline.points.push_back({1, 0.75});
 
-  //double x = 0.4;
-  //double y = 0.075;
-  double xCenter = 0.5;
-  double yCenter = 0.5;
-  double longueur = 0.5;
-  double angle = 0;
-  double anglefixe = (2*PI)/4;
-//"p2" 2->4
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
-//"p3" 5->7
-  m_spline.points.push_back({xCenter+ cos(angle+2*anglefixe)*longueur,yCenter+ sin(angle+2*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+2*anglefixe)*longueur,yCenter+ sin(angle+2*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+2*anglefixe)*longueur,yCenter+ sin(angle+2*anglefixe)*longueur});
-//"p4" 8->10
-  m_spline.points.push_back({xCenter+ cos(angle+3*anglefixe)*longueur,yCenter+ sin(angle+3*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+3*anglefixe)*longueur,yCenter+ sin(angle+3*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+3*anglefixe)*longueur,yCenter+ sin(angle+3*anglefixe)*longueur});
-//"p5" 11->13
-  m_spline.points.push_back({xCenter+ cos(angle+4*anglefixe)*longueur,yCenter+ sin(angle+4*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+4*anglefixe)*longueur,yCenter+ sin(angle+4*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+4*anglefixe)*longueur,yCenter+ sin(angle+4*anglefixe)*longueur});
-//"p6" 14->16
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
-  m_spline.points.push_back({xCenter+ cos(angle+1*anglefixe)*longueur,yCenter+ sin(angle+1*anglefixe)*longueur});
+m_spline.points.push_back({0.75, 1});
+m_spline.points.push_back({0.5, 0.75});
+m_spline.points.push_back({0.5, 0.75});
+
+
+m_spline.points.push_back({0.25, 1});
+m_spline.points.push_back({0, 0.75});
+
+m_spline.points.push_back({0.5, 0});
 
   init();
   metadata().setInstanceName(*this);
