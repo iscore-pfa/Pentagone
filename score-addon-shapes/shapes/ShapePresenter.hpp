7b16bb4f08de8900a910005c6734bc20eb15d732
Anthony <anthofreyer@gmail.com>
2020-04-06 15:35:24 +0200

    Ajout des Presenter mais pas encore dans le CMakeLists

---
+++ score-addon-shapes/shapes/ShapePresenter.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include <Device/Address/AddressSettings.hpp>
+#include <Process/LayerPresenter.hpp>
+
+#include <score/model/path/PathSerialization.hpp>
+
+#include <shapes/ShapeModel.hpp>
+#include <shapes/CommandFactory.hpp>
+
+namespace Shapes
+{
+class ChangeShapes : public score::Command
+{
+  SCORE_COMMAND_DECL(
+      Shapes::CommandFactoryName(),
+      ChangeShapes,
+      "ChangeShapes")
+public:
+  ChangeCircle(
+      const ProcessModel& autom,
+      const ossia::nodes::spline_data& newval)
+      : m_path{autom}, m_old{autom.spline()}, m_new{newval}
+  {
+  }
+
+public:
+  void undo(const score::DocumentContext& ctx) const override
+  {
+    m_path.find(ctx).setShape(m_old);
+  }
+  void redo(const score::DocumentContext& ctx) const override
+  {
+    m_path.find(ctx).setShape(m_new);
+  }
+
+protected:
+  void serializeImpl(DataStreamInput& s) const override
+  {
+    s << m_path << m_old << m_new;
+  }
+  void deserializeImpl(DataStreamOutput& s) override
+  {
+    s >> m_path >> m_old >> m_new;
+  }
+
+private:
+  Path<ProcessModel> m_path;
+  ossia::nodes::spline_data m_old, m_new;
+};
+
+class View;
+class Presenter : public Process::LayerPresenter
+{
+public:
+  explicit Presenter(
+      const Circle::ProcessModel& model,
+      Circle::View* view,
+      const Process::Context& ctx,
+      QObject* parent);
+
+  void setWidth(qreal width, qreal defaultWidth) override;
+  void setHeight(qreal height) override;
+
+  void putToFront() override;
+  void putBehind() override;
+
+  void on_zoomRatioChanged(ZoomRatio) override;
+
+  void parentGeometryChanged() override;
+
+private:
+  const Circle::ProcessModel& m_layer;
+  View* m_view{};
+  ZoomRatio m_zoomRatio{};
+};
+}
